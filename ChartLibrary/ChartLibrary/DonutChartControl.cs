7d8a37cbbd68b9a1cf4c697d2d5f64f8b2a26c11
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2024-05-26 16:17:30 +0300

    Adding ChartLibrary and HelpButtonLibrary

---
+++ ChartLibrary/ChartLibrary/DonutChartControl.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChartLibrary
+{
+    public partial class DonutChartControl : Control
+    {
+        private DonutChartValue[] _data;
+        public DonutChartValue[] Data
+        {
+            get { return _data; }
+            set
+            {
+                _data = value;
+                Invalidate();
+            }
+        }
+        private Color[] _colors;
+        public Color[] Colors
+        {
+            get { return _colors; }
+            set
+            {
+                _colors = value;
+                Invalidate();
+            }
+        }
+        public Font LabelFont { get; set; } = new Font("Sitka Small", 10);
+        public Color LabelColor { get; set; } = Color.Black;
+        public int LegendBoxSize { get; set; } = 20;
+        public int LegendPadding { get; set; } = 10;
+
+        public DonutChartControl()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(this.DonutChartControl_Paint);
+
+            // redraws the chart if the control is resized
+            ResizeRedraw = true;
+
+            // default data
+            Data = new[]
+            {
+                new DonutChartValue("Faculty A", 30),
+                new DonutChartValue("Faculty B", 20),
+                new DonutChartValue("Faculty C", 50)
+            };
+
+            // default colors
+            Colors = new[]
+            {
+                Color.White, Color.Black, Color.Indigo, Color.Purple, Color.MediumPurple
+            };
+        }
+
+        private void DonutChartControl_Paint(object sender, PaintEventArgs e)
+        {
+            // Get the drawing context
+            Graphics graphics = e.Graphics;
+            // Get the drawing area
+            Rectangle clipRectangle = e.ClipRectangle;
+
+            // Ensure the clipRectangle dimensions are valid
+            if (clipRectangle.Width <= 0 || clipRectangle.Height <= 0 || Data == null || Data.Length == 0)
+                return;
+
+            // Calculate the total value of the data
+            float totalValue = Data.Sum(d => d.Value);
+
+            // Set the center and radius for the donut chart
+            PointF center = new PointF(clipRectangle.Width / 3, clipRectangle.Height / 2);
+            float radius = Math.Min(clipRectangle.Width / 2, clipRectangle.Height / 2) * 0.8f;
+            float innerRadius = radius * 0.5f;
+
+            // Ensure the radius dimensions are valid
+            if (radius <= 0 || innerRadius <= 0)
+                return;
+
+            // Starting angle for the first slice
+            float startAngle = 0;
+
+            for (int i = 0; i < Data.Length; i++)
+            {
+                // Calculate the sweep angle for each slice
+                float sweepAngle = (Data[i].Value / totalValue) * 360;
+
+                using (Brush brush = new SolidBrush(Colors[i % Colors.Length]))
+                {
+                    // Draw the outer arc
+                    graphics.FillPie(brush,
+                        center.X - radius,
+                        center.Y - radius,
+                        2 * radius,
+                        2 * radius,
+                        startAngle,
+                        sweepAngle);
+                }
+
+                // Draw the inner circle to create the donut effect
+                using (Brush brush = new SolidBrush(this.BackColor))
+                {
+                    graphics.FillEllipse(brush,
+                        center.X - innerRadius,
+                        center.Y - innerRadius,
+                        2 * innerRadius,
+                        2 * innerRadius);
+                }
+
+                // Update the start angle for the next slice
+                startAngle += sweepAngle;
+            }
+
+            // Draw the legend
+            DrawLegend(graphics, clipRectangle);
+        }
+
+        private void DrawLegend(Graphics graphics, Rectangle clipRectangle)
+        {
+            int legendX = clipRectangle.Width - (LegendBoxSize + LegendPadding * 2 + 100); // Adjust for text width
+            int legendY = LegendPadding;
+            int boxSize = LegendBoxSize;
+
+            for (int i = 0; i < Data.Length; i++)
+            {
+                using (Brush brush = new SolidBrush(Colors[i % Colors.Length]))
+                {
+                    // Draw legend color box
+                    graphics.FillRectangle(brush, legendX, legendY, boxSize, boxSize);
+                }
+
+                // Draw legend text
+                string legendText = $"{Data[i].Label}: {Data[i].Value}%";
+                using (Brush textBrush = new SolidBrush(LabelColor))
+                {
+                    // Measure the string to adjust position
+                    SizeF stringSize = graphics.MeasureString(legendText, LabelFont);
+                    graphics.DrawString(legendText, LabelFont, textBrush, legendX + boxSize + LegendPadding, legendY + (boxSize - stringSize.Height) / 2);
+                }
+
+                // Update the Y position for the next legend item
+                legendY += boxSize + LegendPadding;
+            }
+        }
+    }
+}
---
+++ HelpButtonLibrary/HelpButtonLibrary/HelpButtonControl.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace HelpButtonLibrary
+{
+    partial class HelpButtonControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HelpButtonControl));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbExitHelp = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExitHelp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumOrchid;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(379, 49);
+            this.panel1.TabIndex = 0;
+            // 
+            // pbExitHelp
+            // 
+            this.pbExitHelp.Image = ((System.Drawing.Image)(resources.GetObject("pbExitHelp.Image")));
+            this.pbExitHelp.Location = new System.Drawing.Point(270, 3);
+            this.pbExitHelp.Name = "pbExitHelp";
+            this.pbExitHelp.Size = new System.Drawing.Size(29, 27);
+            this.pbExitHelp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbExitHelp.TabIndex = 2;
+            this.pbExitHelp.TabStop = false;
+            this.pbExitHelp.Click += new System.EventHandler(this.pbExitHelp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(78, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Commands and instructions";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 43);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel2.Controls.Add(this.pbExitHelp);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(25, 83);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(302, 207);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Indigo;
+            this.panel3.Controls.Add(this.label11);
+            this.panel3.Controls.Add(this.label10);
+            this.panel3.Location = new System.Drawing.Point(0, 143);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(302, 64);
+            this.panel3.TabIndex = 8;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(3, 35);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(287, 19);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Transcripts can be serialized/deserialized";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(3, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(266, 19);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Transcripts can be exported as txt files";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(94, 104);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 19);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Alt+C";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(94, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 19);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Alt+D";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(94, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 19);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Alt+U";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(94, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 19);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Alt+S";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(3, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 19);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Clear?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(3, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 19);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Delete?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 19);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Update?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Save?";
+            // 
+            // HelpButtonControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(226)))), ((int)(((byte)(254)))));
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "HelpButtonControl";
+            this.Size = new System.Drawing.Size(379, 330);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExitHelp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.PictureBox pbExitHelp;
+    }
+}
